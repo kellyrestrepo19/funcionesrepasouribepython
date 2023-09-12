3a1f032905171313287c700b245b8f8076c2d3bd
Kelly Muñoz <kelly19011@gmail.com>
2023-09-12 07:05:07 -0500

    Se crean

---
+++ funcion1.py
@@ -0,0 +1,6 @@
+# funcion que reciba el nombre de una presona y la salude
+def saludarpersona(nombre):
+    print(f"Hola buenos dias dormilona {nombre}")
+    
+# Para usar una funcion hay que llamarla 
+saludarpersona("Kelly Muñoz")
